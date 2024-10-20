e992627c30e6004255b325655022309c22fa6212
Ananthu PM <ananthupmadhu@gmail.com>
2024-10-20 15:55:04 +0530

    Pattern Syntax Checker

--- programming/hackerrank/java/Java-String-Tokens/src/main/java/Solution.java
+++ programming/hackerrank/java/Java-String-Tokens/src/main/java/Solution.java
@@ -7,7 +7,12 @@ public class Solution {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
         // Write your code here.
-        String[] out = s.split("[\\s!,?._'@]+");
+        if(s==null || s.trim().equals("")) {
+            System.out.println("0");
+            scan.close();
+            return;
+        }
+        String[] out = s.trim().split("[\\n\\s\\t !,?._'@]+");
         System.out.println(out.length);
         for(int i=0;i<out.length;i++){
             System.out.println(out[i]);
